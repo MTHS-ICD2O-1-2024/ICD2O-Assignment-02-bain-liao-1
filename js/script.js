cfe46a7cc5c848070fe4aac6585737f68a015c28
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-03-24 21:12:07 +0000

    created program that calculates the area of a parallelogram

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bain Liao
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates the area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateAreaOfParallelogram () {
+  // input
+  const baseOfParallelogram = parseFloat(document.getElementById('base-of-parallelogram').value)
+  const heightOfParallelogram = parseFloat(document.getElementById('height-of-parallelogram').value)
+
+  // process
+  const areaOfParallelogram = baseOfParallelogram * heightOfParallelogram
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfParallelogram + ' cm²'
 }
